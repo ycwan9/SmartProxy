601adc600390f7737774d81846659fb7c6ce7c85
Messiah Luo <luoyongmao@gmail.com>
2015-03-18 23:06:51 +0800

    socket连接释放后释放encryptor资源

--- Smart Proxy X/src/main/java/me/smartproxy/tunnel/shadowsocks/ShadowsocksEncryptor.java
+++ Smart Proxy X/src/main/java/me/smartproxy/tunnel/shadowsocks/ShadowsocksEncryptor.java
@@ -11,14 +11,19 @@ public class ShadowsocksEncryptor implements IEncryptor {
 
     private long id;
 
+    private volatile boolean isEncrypting , isDecrypting;
+
     public ShadowsocksEncryptor(String password, String method) {
         id = new Random(System.currentTimeMillis()).nextLong();
         CryptoUtils.initEncryptor(password, method, id);
+        isEncrypting = false;
+        isDecrypting = false;
     }
 
     @Override
     public void encrypt(ByteBuffer buffer) {
         if (buffer.hasRemaining()) {
+            isEncrypting = true;
             int bufferSize = buffer.remaining();
             byte[] encryptionBuff = new byte[bufferSize];
             buffer.get(encryptionBuff, buffer.position(), buffer.remaining());
@@ -26,23 +31,32 @@ public class ShadowsocksEncryptor implements IEncryptor {
             buffer.clear();
             buffer.put(cipher);
             buffer.flip();
+            isEncrypting = false;
         }
     }
 
     @Override
     public void decrypt(ByteBuffer buffer) {
         if (buffer.hasRemaining()) {
+            isDecrypting = true;
             byte[] decryptBuff = new byte[buffer.remaining()];
             buffer.get(decryptBuff, buffer.position(), buffer.remaining());
             byte[] result = CryptoUtils.decrypt(decryptBuff, id);
             buffer.clear();
             buffer.put(result);
             buffer.flip();
+            isDecrypting = false;
         }
     }
 
     @Override
     public void dispose() {
-        CryptoUtils.releaseEncryptor(id);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(isEncrypting || isDecrypting);
+                CryptoUtils.releaseEncryptor(id);
+            }
+        }).start();
     }
 }
