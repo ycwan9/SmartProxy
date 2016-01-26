49eab623b5e563c2288e5c985e0ba0525015995c
ycwna9 <ycwan9@outlook.com>
2016-01-26 21:50:23 +0800

    add socks5 proxy support without authentication
    use `proxy socks5://<server_ip>:<server_port>`

--- Smart Proxy X/src/main/java/me/smartproxy/tunnel/socks5/Socks5Tunnel.java
+++ Smart Proxy X/src/main/java/me/smartproxy/tunnel/socks5/Socks5Tunnel.java
@@ -7,25 +7,27 @@ import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.text.Bidi;
 
+import me.smartproxy.core.LocalVpnService;
 import me.smartproxy.core.ProxyConfig;
 import me.smartproxy.tunnel.Tunnel;
 
 public class Socks5Tunnel extends Tunnel {
 
-	private boolean m_TunnelEstablished;
 	private Socks5Config m_Config;
+	private int stage;
 
 	public Socks5Tunnel(Socks5Config config,Selector selector) throws IOException {
 		super(config.ServerAddress,selector);
 		m_Config=config;
+		LocalVpnService.Instance.writeLog("new socks5 tunnel start server <%s>", config.ServerAddress.toString());
+		stage = 1;
 	}
 
 	@Override
 	protected void onConnected(ByteBuffer buffer) throws Exception {
 		try{
-			byte[] b = new byte[2];
+			//byte[] b = new byte[2];
 			String request1 = "\005\001\000";
-			String request2 = "\005\001\000\001";
 			//\x05 socks v5
 			//\x01 nmethods 1
 			//\x00 methods 00 (no auth)
@@ -33,39 +35,13 @@ public class Socks5Tunnel extends Tunnel {
 			buffer.put(request1.getBytes());
 			buffer.flip();
 			if (!this.write(buffer,true)){
-				throw new Exception("socks5 connect 1st failed");
+				throw new Exception("Err socks5 connect 1st failed");
 			}
 			buffer.clear();
-			m_InnerChannel.configureBlocking(true);
-			m_InnerChannel.read(buffer);
-			buffer.flip();
-			buffer.get(b);
-			if (!((b[0]==5)&&(b[1]==0))) {
-				throw new Exception(new String.format("failed to auth <\\x%x\\x%x>",b[0],b[1]));
-			}
-			buffer.clear();
-			buffer.put(request2.getBytes());
-			buffer.put(m_DestAddress.getAddress().getAddress());
-			buffer.put((byte)(0xff00 & m_DestAddress.getPort()));//send 
-			buffer.put((byte)(0x00ff & m_DestAddress.getPort()));//htons(port)
-			buffer.flip();
-			if(!this.write(buffer,true)){
-				throw new Exception("socks5 CMD connect failed to send");
-			}
-			buffer.clear();
-			m_InnerChannel.configureBlocking(true);
-			m_InnerChannel.read(buffer);
-			buffer.flip();
-			buffer.get(b);
-			if (!((b[0]==5)&&(b[1]==0))) {
-				throw new Exception(new String.format("failed to connect <\\x%x\\x%x>",b[0],b[1]));
-			}
+			this.beginReceive();
 		} catch (Exception e) {
-			throw new Exception(new String.format("Exception while connect to socks5 server <%s>",e));
+			throw new Exception(String.format("Err Exception while connect to socks5 server <%s>",e));
 		}
-		this.beginReceive();
-		m_TunnelEstablished = true;
-		super.onTunnelEstablished();
 	}
 
 
@@ -73,13 +49,76 @@ public class Socks5Tunnel extends Tunnel {
 	protected void beforeSend(ByteBuffer buffer) throws Exception {
 	}
 
+	void sendConnectCmd(ByteBuffer buffer) throws Exception {
+		//DBG-LocalVpnService.Instance.writeLog("get into sendConnectCmd()");
+		//DBG-LocalVpnService.Instance.writeLog("address is <%s>", m_DestAddress.toString());
+		//String request2 = "\005\001\000";
+		byte[] Request2CmdHeader = new byte[]{'\005', '\001', '\000'};
+		//\x05 version 5
+		//\x01 CMD CONNECT
+		//\x00 RSV
+		//\x03 FQDN
+		buffer.clear();
+		buffer.put(Request2CmdHeader);
+		try {
+			byte[] Addr  = new byte[4];
+			String[] StrAddr = m_DestAddress.getHostName().split("\\.");
+			for (int i = 0; i < 4; i++) {
+				Addr[i] = (byte) Integer.parseInt(StrAddr[i]);
+			}
+			//ipv4 address
+			buffer.put((byte) '\001');
+			buffer.put(Addr);
+		} catch (Exception e) {
+			//FQDN
+			buffer.put((byte) '\003');
+			String hostname = m_DestAddress.getHostName();
+			buffer.put((byte)hostname.length());
+			buffer.put(hostname.getBytes());
+		}
+		buffer.put((byte)(m_DestAddress.getPort() >>> 8));//send
+		buffer.put((byte)(0x00ff & m_DestAddress.getPort()));//htons(port)
+		buffer.flip();
+		//DBG-LocalVpnService.Instance.writeLog("buffer flip to send");
+		if(!this.write(buffer,true)){
+			throw new Exception("Err socks5 CMD connect failed to send");
+		}
+	}
+
 	@Override
 	protected void afterReceived(ByteBuffer buffer) throws Exception {
+		//String dbg = new String(buffer.array());
+		//LocalVpnService.Instance.writeLog(dbg);
+		if (stage!=0) {
+			//DBG-LocalVpnService.Instance.writeLog("socks5 afterRecv auth stage %d",stage);
+			byte[] b = new byte[2];
+			buffer.get(b);
+			//DBG-LocalVpnService.Instance.writeLog("socks5 handshake stage %d had <\\x%x\\x%x>", stage, b[0], b[1]);
+			if (!((b[0]==5)&&(b[1]==0))) {
+				throw new Exception(String.format("Err failed to auth on stage %d <\\x%x\\x%x>", stage, b[0], b[1]));
+			}
+			try {
+				if (stage == 1) {
+					//DBG-LocalVpnService.Instance.writeLog("socks5 handshake stage 01 get into if()");
+					this.sendConnectCmd(buffer);
+					buffer.clear();
+					stage = 2;
+				} else if (stage == 2) {
+					//DBG-LocalVpnService.Instance.writeLog("socks5 handshake stage 02 get into if()");
+					//buffer.clear();
+					buffer.limit(buffer.position());
+					super.onTunnelEstablished();
+					stage = 0;
+				}
+			} catch (Exception e) {
+				LocalVpnService.Instance.writeLog("Exception on handshake err <%s> in stage <%d>",e,stage);
+			}
+		}
 	}
 
 	@Override
 	protected boolean isTunnelEstablished() {
-		return m_TunnelEstablished;
+		return (stage == 0);
 	}
 
 	@Override
