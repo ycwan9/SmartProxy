791528445a55a8796cadbbe74d59bd20f935551e
A.I <merrick@luois.me>
2016-08-20 21:54:33 +0800

    send http proxy basic auth info when connect

--- app/src/main/java/me/smartproxy/tunnel/httpconnect/HttpConnectTunnel.java
+++ app/src/main/java/me/smartproxy/tunnel/httpconnect/HttpConnectTunnel.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.Selector;
 
+import android.util.Base64;
+
 import me.smartproxy.core.ProxyConfig;
 import me.smartproxy.tunnel.Tunnel;
 
@@ -20,12 +22,7 @@ public class HttpConnectTunnel extends Tunnel {
 
     @Override
     protected void onConnected(ByteBuffer buffer) throws Exception {
-        String request = String
-                .format("CONNECT %s:%d HTTP/1.0\r\nProxy-Connection: keep-alive\r\nUser-Agent: %s\r\nX-App-Install-ID: %s\r\n\r\n",
-                        m_DestAddress.getHostName(),
-                        m_DestAddress.getPort(),
-                        ProxyConfig.getInstance().getUserAgent(),
-                        ProxyConfig.AppInstallID);
+        String request = buildNewRequest();
 
         buffer.clear();
         buffer.put(request.getBytes());
@@ -33,6 +30,21 @@ public class HttpConnectTunnel extends Tunnel {
         if (this.write(buffer, true)) {//发送连接请求到代理服务器
             this.beginReceive();//开始接收代理服务器响应数据
         }
+    }
+
+    private String buildNewRequest() {
+        String request = String
+                .format("CONNECT %s:%d HTTP/1.0\r\nProxy-Connection: keep-alive\r\nUser-Agent: %s\r\nX-App-Install-ID: %s\r\n",
+                        m_DestAddress.getHostName(),
+                        m_DestAddress.getPort(),
+                        ProxyConfig.getInstance().getUserAgent(),
+                        ProxyConfig.AppInstallID);
+        if (m_Config.UserName != null && m_Config.Password != null) {
+            byte[] authBytes = (m_Config.UserName + ':' + m_Config.Password).getBytes();
+            String base64Auth = Base64.encodeToString(authBytes, Base64.CRLF);
+            request = String.format("%sProxy-Authorization: Basic %s", request, base64Auth);
+        }
+        return request + "\r\n";
     }
 
     void trySendPartOfHeader(ByteBuffer buffer) throws Exception {
